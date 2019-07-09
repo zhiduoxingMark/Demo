5fa26cb451eb6527727060af7eb0757af315fa67
wuxiaoqin <158990807@qq.com>
2019-07-09 22:09:30 +0800

    demo

---
+++ Demo/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly:Autodesk.AutoCAD.Runtime.CommandClass(typeof(Demo.Class1))]
+namespace Demo
+{
+    public class Class1
+    {
+        [Autodesk.AutoCAD.Runtime.CommandMethod("tool1")]
+        public void Tool1()
+        {
+            Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage("Hello World");
+        }
+    }
+}
